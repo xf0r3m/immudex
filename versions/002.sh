8c2bb8c1260a2f89264ec591d095cf0a36774a79
xf0r3m <jakubstasinski@protonmail.com>
2022-10-22 16:41:03 +0200

    Przygotowanie do wydania 0.0.2 - część 5

--- versions/002.sh
+++ versions/002.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+
+version=$(echo $0 | cut -d "." -f 1);
+if [ ! "$version" ]; then echo -e "\e[31mUpdate failed!\e[0m"; exit 1; fi;
+
+dhclient
 sudo apt update;
 sudo apt upgrade -y;
+sudo apt install -y chirp mpv youtube-dl;
 
 lineNumber=$(cat -n /etc/bash.bashrc | grep 'motd2' | awk '{printf $1}');
 if [ "$lineNumber" ]; then
@@ -18,18 +24,23 @@ sudo usermod -aG libvirt,libvirt-qemu xf0r3m;
 
 sudo chmod u+s /usr/bin/ping
 
-sudo cp ~/immudex/tools/002/motd2 /usr/local/bin
-sudo cp ~/immudex/tools/002/padlock /usr/local/bin
-sudo cp ~/immudex/tools/002/import_gpgkeys /usr/local/bin
-sudo cp ~/immudex/tools/002/shoutcasts /usr/local/bin
-sudo cp ~/immudex/tools/002/ytaudio /usr/local/bin
+if [ ! -d ~/immudex ]; then
+  cd;
+  git clone https://github.com/xf0r3m/immudex;
+fi
+
+sudo cp ~/immudex/tools/${version}/motd2 /usr/local/bin
+sudo cp ~/immudex/tools/${version}/padlock /usr/local/bin
+sudo cp ~/immudex/tools/${version}/import_gpgkeys /usr/local/bin
+sudo cp ~/immudex/tools/${version}/shoutcasts /usr/local/bin
+sudo cp ~/immudex/tools/${version}/ytaudio /usr/local/bin
 sudo cp ~/immudex/tools/immudex_install /usr/local/bin
 
 chmod +x /usr/local/bin/*
 
-sudo apt install chirp mpv;
 sudo sed -i -e 's/chirpw/sudo chirpw' -e 's/false/true' /usr/share/applications/chirp.desktop;
 
+rm -rf ~/immudex;
 sudo apt-get clean;
 echo > ~/.bash_history;
 history -c;
