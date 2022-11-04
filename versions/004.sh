e2db6c4ff24f89d184bbbe1f80d3f401540dfd79
xf0r3m <jakubstasinski@protonmail.com>
2022-11-04 18:23:40 +0100

    Poprawienie błędu w pliku wersji

--- versions/004.sh
+++ versions/004.sh
@@ -18,7 +18,7 @@ sudo rm /etc/skel/.config/xfce4/panel/launcher-32/16608177609.desktop
 sudo cp -vv ~/immudex/files/${version}/xfce4-panel.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
 sudo cp -vv ~/immudex/files/${version}/redshift.conf /etc/skel/.config;
 sudo mkdir -v /etc/skel/.config/autostart;
-sudo cp -vv ~/immudex/filex/${version}/redshift.desktop /etc/skel/.config/autostart;
+sudo cp -vv ~/immudex/files/${version}/redshift.desktop /etc/skel/.config/autostart;
 
 sudo userdel -r user;
 sudo userdel -r xf0r3m;
