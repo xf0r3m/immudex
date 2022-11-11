03c591c6adaa08289da09c86d7c2aa445900ca1d
xf0r3m <jakubstasinski@protonmail.com>
2022-11-11 13:47:24 +0100

    Przygotowanie do wydania 0.0.5 - część 2

--- versions/005.sh
+++ versions/005.sh
@@ -9,21 +9,23 @@ sudo apt upgrade -y;
 
 sudo apt install -y libssl-dev libbz2-dev libnss3-dev libgdbm-dev libncurses5-dev libffi-dev libreadline-dev libsqlite3-dev zlib1g-dev build-essential
 
-sudo apt install --no-install-recommends software-properties-common
+sudo apt install --no-install-recommends software-properties-common -y
 
 sudo ln -s /etc/libvirt/qemu/networks/default.xml /etc/libvirt/qemu/networks/autostart/default.xml;
 
 cd;
 sudo wget https://www.python.org/ftp/python/3.11.0/Python-3.11.0.tar.xz -O Python-3.11.0.tar.xz;
 sudo tar -xvf Python-3.11.0.tar.xz;
-cd Python-3.11.0.tar.xz;
+cd Python-3.11.0;
 
 sudo ./configure --prefix=/usr/local --enable-optimizations
 sudo make
 sudo make altinstall
 
-sudo sed -i '26s/1/0/' /etc/skel/.config/xfce4/xconf/xfce-perchannel-xml/xfce4-panel.xml;
-sudo sed -i '64i\\t<property name="disable-struts" type="bool" value="false"/>' /etc/skel/.conf/xfce4/xconf/xfce-perchannel-xml/xfce4-panel.xml;
+sudo sed -i '26s/1/0/' /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml;
+sudo sed -i '64i\\t<property name="disable-struts" type="bool" value="false"/>' /etc/skel/.conf/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml;
+
+echo 'alias ytstream="mpv --ytdl-format=best[height=480]"' >> /etc/bash.bashrc;
 
 sudo userdel -r user;
 sudo userdel -r xf0r3m;
