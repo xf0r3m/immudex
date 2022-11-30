27eeab204c0f40c308f0c6312f8d0c10821d78ed
xf0r3m <jakubstasinski@protonmail.com>
2022-11-30 23:45:08 +0100

    Przygotowania do publikacji immudex 0.0.7 - część 3

--- versions/007.sh
+++ versions/007.sh
@@ -7,7 +7,7 @@ sudo dhclient;
 sudo apt update;
 sudo apt upgrade -y;
 
-sudo apt install transmission-gtk;
+sudo apt install -y transmission-gtk;
 
 
 sudo cp -vv ~/immudex/tools/${version}/ytplay /usr/local/bin;
@@ -16,8 +16,26 @@ chmod +x /usr/local/bin;
 
 sudo rm -rf /etc/skel/.mozilla;
 sudo tar -xzvf ~/immudex/files/${version}/mozilla.tgz -C /etc/skel
+sudo chown -R root:root /etc/skel/.mozilla
 
-
+sudo sed -i 's/80x24/100x30/' /etc/skel/.config/xfce4/terminal/terminalrc
 
 sudo apt remove -y libssl-dev libbz2-dev libnss3-dev libgdbm-dev libncurses5-dev libffi-dev libreadline-dev libsqlite3-dev zlib1g-dev build-essential software-properties-common;
 sudo apt autoremove -y;
+
+sudo userdel -r user;
+sudo userdel -r xf0r3m;
+sudo useradd -m -s /bin/bash user;
+sudo useradd -m -s /bin/bash xf0r3m;
+echo 'user:user1' | sudo chpasswd user;
+echo 'xf0r3m:xf0r3m1' | sudo chpasswd xf0r3m;
+sudo usermod -aG libvirt,libvirt-qemu user;
+sudo usermod -aG libvirt,libvirt-qemu xf0r3m;
+
+rm -rf ~/immudex;
+sudo apt autoclean;
+sudo apt-get clean;
+sudo apt-get clean;
+echo > ~/.bash_history;
+history -c
+
