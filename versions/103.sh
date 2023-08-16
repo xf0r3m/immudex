65cdd6298cae38c9c647c451400c6809f58538f5
xf0r3m <jakubstasinski@protonmail.com>
2023-08-16 11:32:39 +0200

    Przygotowanie do wydania immudex w wersji 1.0.3 - część 1

---
+++ versions/103.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dhclient;
+cd
+if [ -x /usr/bin/git ]; then git clone https://github.com/xf0r3m/immudex.git;
+else apt install git && git clone https://github.com/xf0r3m/immudex.git;
+fi
+source ~/immudex/versions/template.sh;
+
+update_packages;
+apt purge -y xpra;
+
+cp -vv ~/immudex/tools/${VERSION}/pl /usr/local/bin;
+cp -vv ~/immudex/tools/${VERSION}/idle-clic /usr/local/bin;
+
+cp -vv ~/immudex/files/${VERSION}/xfce4-keyboard-shortcuts.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml;
+cp -vv ~/immudex/files/${VERSION}/xfwm4.xml /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml;
+
+recreate_users;
+
+set_mime;
+
+tidy;
+
