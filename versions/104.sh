af914237718aef6d41c2e936390972b0c3129a71
xf0r3m <jakubstasinski@protonmail.com>
2023-09-12 16:21:00 +0200

    Przygotowanie do wydania immudex w wersji 1.0.4 - część 2

--- versions/104.sh
+++ versions/104.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 ARCH=$(dpkg --print-architecture);
-dhclient;
 cd
-if [ -x /usr/bin/git ]; then git clone https://github.com/xf0r3m/immudex.git;
-else apt install git && git clone https://github.com/xf0r3m/immudex.git;
+if [ -x /usr/bin/git ]; then git clone https://git.morketsmerke.org/git/immudex;
+else apt install git && git clone https://git.morketsmerke.org/git/immudex
 fi
 source ~/immudex/versions/template.sh;
 
@@ -22,8 +21,20 @@ fi
 ln -s /usr/lib/librewolf/librewolf /usr/bin/librewolf;
 
 cp -vv ~/immudex/files/${VERSION}/LibreWolf\ \(Firejailed\).desktop /etc/skel/Pulpit;
-rm -v /etc/skel/Pulpit/Frefox.desktop;
+rm -v /etc/skel/Pulpit/Firefox.desktop;
 
+cp -vv ~/immudex/files/${VERSION}/terminalrc /etc/skel/.config/xfce4/terminal;
+cp -vv ~/immudex/files/${VERSION}/librewolf.desktop /usr/share/applications;
+cp -vv ~/immudex/files/${VERSION}/gtk-main.css /usr/share/xfce4-notes-plugin/gtk-3.0;
+tar -xf ~/immudex/files/${VERSION}/librewolf.tgz -C /etc/skel;
+
+cp -vv ~/immudex/tools/${VERSION}/secured-firefox /usr/local/bin;
+
+cp -vv ~/immudex/images/${VERSION}/lightdm_wallpaper.jpg /usr/share/images/desktop-base;
+sed -i 's/abandoned_hospital_bower.png/lightdm_wallpaper.jpg/' /etc/lightdm/lightdm-gtk-greeter.conf;
+rm -v /usr/share/images/desktop-base/abandoned_hospital_bower.png;
+rm -v /usr/share/images/desktop-base/lofi_girl.jpg;
+rm -v /usr/share/images/dektop-base/notes-background.jpg;
 recreate_users;
 
 set_mime;
