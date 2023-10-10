8403804b4b65004cb1c21e0c51b9962305aa0c4b
xf0r3m <jakubstasinski@protonmail.com>
2023-10-10 18:29:26 +0200

    Poprawienie pliku wersji

--- versions/105.sh
+++ versions/105.sh
@@ -2,8 +2,8 @@
 
 ARCH=$(dpkg --print-architecture);
 cd
-if [ -x /usr/bin/git ]; then git clone https://git.morketsmerke.org/git/immudex;
-else apt install git && git clone https://git.morketsmerke.org/git/immudex
+if [ -x /usr/bin/git ]; then git clone https://github.com/xf0r3m/immudex;
+else apt install git && git clone https://github.com/xf0r3m/immudex;
 fi
 source ~/immudex/versions/template.sh;
 
@@ -11,8 +11,8 @@ update_packages;
 
 if [ $ARCH = "amd64" ]; then
   wget https://ftp.morketsmerke.org/immudex/testing/software/librewolf/librewolf-118.0.1-1.en-US.linux-x86_64.tar.bz2;
-  tar -xf -C /usr/lib;
-  rm ;
+  tar -xf librewolf-118.0.1-1.en-US.linux-x86_64.tar.bz2 -C /usr/lib;
+  rm librewolf-118.0.1-1.en-US.linux-x86_64.tar.bz2;
 else
   wget https://ftp.morketsmerke.org/immudex/testing/software/librewolf/librewolf-118.0.1-1.en-US.linux-i686.tar.bz2;
   tar -xf librewolf-118.0.1-1.en-US.linux-i686.tar.bz2 -C /usr/lib;
