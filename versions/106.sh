8f28b61b9e3bc264d2e8c47a6c3e3dd7f7a5e1f8
xf0r3m <jakubstasinski@protonmail.com>
2023-10-12 18:09:07 +0200

    Przygotowanie do wydania immudex wersji 1.0.6 - część 1

---
+++ versions/106.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd
+if [ -x /usr/bin/git ]; then git clone https://github.com/xf0r3m/immudex;
+else apt install git && git clone https://github.com/xf0r3m/immudex;
+fi
+source ~/immudex/versions/template.sh;
+
+update_packages;
+
+cd;
+mkdir curl;
+cd curl;
+wget https://github.com/curl/curl/archive/refs/tags/curl-8_4_0.tar.gz;
+tar -xf curl-8_4_0.tar.gz;
+cd curl-curl-8_4_0;
+sudo apt install -y zlib1g-dev libc6-dev libnghttp2-dev libidn2-0-dev librtmp-dev libssh2-1-dev libpsl-dev libssh2-1-dev libssl-dev libgss-dev libldap-dev libzstd-dev libbrotli-dev libgnutls28-dev libhogweed6 nettle-dev libgmp-dev libkrb5-dev libk5crypto3 libcom-dev libkrb5support0 libsasl2-dev libp11-dev libtasn1-6-dev libkeyutils-dev libffi-dev autoconf libtool
+autoreconf -fi
+./configure --with-openssl --with-gssapi --disable-libcurl-option --with-libssh2
+make
+make install
+rm /usr/bin/curl
+cp -vv /usr/local/lib/libcurl.* /lib/x86_64-linux-gnu/
+ln -s /usr/local/bin/curl /usr/bin/curl;
+curl -V;
+cd;
+rm -rf curl;
+
+set_mime;
+
+tidy;
+
